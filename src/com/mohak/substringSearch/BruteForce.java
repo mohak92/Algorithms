808785e780bb0de885e35020995c24c7f83d1452
mohak92 <tamhane.mohak@gmail.com>
2020-05-04 10:44:08 -0400

    Brute Force substring search implementation

---
+++ src/com/mohak/substringSearch/BruteForce.java
@@ -0,0 +1,33 @@
+package com.mohak.substringSearch;
+
+public class BruteForce {
+
+	public static void main(String[] args) {
+		
+		String text = "My name is john";
+		String pattern = "name";
+		
+		System.out.println(search(text, pattern));
+
+	}
+	
+	public static int search(String text, String pattern) {
+		int lengthOfText = text.length();
+		int lengthOfPattern = pattern.length();
+		
+		for(int i = 0;i <= lengthOfText - lengthOfPattern; i++){
+			
+			int j;
+			
+			for(j = 0;j < lengthOfPattern;j++) {
+				if(text.charAt(i+j) != pattern.charAt(j)) {
+					break;
+				}
+			}
+			if(j == lengthOfPattern) return i;
+		}
+		
+		return lengthOfText;
+	}
+
+}
